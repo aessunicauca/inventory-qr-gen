d0de31e4d3db854ba78b89e0f89374997fa536c0
Camilo Segura <camilo.segura@itconsultores.com.co>
2023-03-15 03:54:05 -0500

    [FEATURE] Excel parser finished. Waiting to improve categories. QR now rendering whole page. Waiting for sticker configuration and PDF type rendering

--- QR.py
+++ QR.py
@@ -4,27 +4,6 @@ from PIL import Image
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 
-# Diccionario de URLs
-# urls_imagenes = {
-#     "imagen1": "https://ejemplo.com/imagen1.jpg",
-#     "imagen2": "https://ejemplo.com/imagen2.jpg",
-#     "imagen3": "https://ejemplo.com/imagen3.jpg",
-#     "imagen4": "https://ejemplo.com/imagen4.jpg",
-#     "imagen5": "https://ejemplo.com/imagen5.jpg",
-#     "imagen6": "https://ejemplo.com/imagen6.jpg",
-#     "imagen7": "https://ejemplo.com/imagen7.jpg",
-#     "imagen8": "https://ejemplo.com/imagen8.jpg",
-#     "imagen9": "https://ejemplo.com/imagen9.jpg",
-#     "imagen10": "https://ejemplo.com/imagen10.jpg",
-#     "imagen13": "https://ejemplo.com/imagen3.jpg",
-#     "imagen14": "https://ejemplo.com/imagen4.jpg",
-#     "imagen15": "https://ejemplo.com/imagen5.jpg",
-#     "imagen16": "https://ejemplo.com/imagen6.jpg",
-#     "imagen17": "https://ejemplo.com/imagen7.jpg",
-#     "imagen18": "https://ejemplo.com/imagen8.jpg",
-#     "imagen19": "https://ejemplo.com/imagen9.jpg",
-#     "imagen20": "https://ejemplo.com/imagen10.jpg"
-# }
 
 # Función para generar QR y agregarlo al PDF
 def generar_qr(url, qr_size, pdf, x, y, micro=False):
@@ -36,26 +15,26 @@ def generar_qr(url, qr_size, pdf, x, y, micro=False):
     qr.save(out, scale=5, kind='png')
     out.seek(0)
     img = Image.open(out)
-    pdf.drawInlineImage(img, x, y, qr_size*4, qr_size*4)
+    pdf.drawInlineImage(img, x, y, qr_size, qr_size)
 
-def generar_pagina(urls_imagenes):
+def generar_pagina(df):
     # Parámetros de entrada
-    qr_size = 10  # Tamaño del QR (en celdas)
-    filename = "urls_imagenes.pdf"  # Nombre del archivo PDF
-    x_start = 50  # Coordenada x de inicio
-    y_start = 50  # Coordenada y de inicio
+    qr_size = 30  # Tamaño del QR (en celdas)
+    x_start = 20  # Coordenada x de inicio
+    y_start = 740  # Coordenada y de inicio
     x = x_start
     y = y_start
 
     # Crear el archivo PDF y generar QR para cada URL en el diccionario
-    pdf = canvas.Canvas(filename, pagesize=letter)
-    for value in urls_imagenes:
-        generar_qr(value, qr_size, pdf, x, y, micro=False)
-        x += qr_size*6  # Avanzar en la coordenada x
-        if x > 550:  # Saltar a la siguiente línea si se llega al límite de la página
+    pdf_small = canvas.Canvas("stickers_small.pdf", pagesize=letter)
+    pdf_large = canvas.Canvas("stickers_large.pdf", pagesize=letter)
+    for i in range(df.shape[0]):
+        generar_qr(df["url"][i], qr_size, pdf_small, x, y, micro=False)
+        x += qr_size  # Avanzar en la coordenada x
+        if x > 580:  # Saltar a la siguiente línea si se llega al límite de la página
             x = x_start
-            y -= qr_size*6
-            if y < 50:  # Crear una nueva página si se llega al límite de la página
-                pdf.showPage()
+            y -= qr_size
+            if y < 20:  # Crear una nueva página si se llega al límite de la página
+                pdf_small.showPage()
                 y = y_start
-    pdf.save()
+    pdf_small.save()
--- updateExcel.py
+++ updateExcel.py
@@ -4,53 +4,40 @@ import random
 import string
 from QR import *
 
+# TODO: split in modular functions as some of this def will only be used once
+# TODO: create easy functions on normal use cases like regenerating PDFs from Excel file (useful if the org receives new stuff)
+
 #asignar codigo unico alfanumerico de de tamaño 7
 def insert_code_unique(df, code_size=5):
     for i in range(df.shape[0]):
-        while True:
-            # Generar código aleatorio
-            code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=code_size))
-            
-            # Verificar si el código ya existe en la tabla
-            if not df["code"][i] == code:
-                # Asignar el código a la fila
-                df["code"][i] = code
-                break
+        if pd.isna(df["code"][i]):
+            while True:
+                # Generar código aleatorio
+                code = ''.join(random.choices(string.ascii_uppercase + \
+                                            string.ascii_lowercase + string.digits, k=code_size))
+                
+                # Verificar si el código ya existe en la tabla
+                if not df["code"][i] == code:
+                    # Asignar el código a la fila
+                    df["code"][i] = code
+                    break
 
 def insert_code_category(df):
     # Cargar tabla de Categorías
-    categorias_df = pd.read_excel('inventario.xlsx', sheet_name='Categorías')
+    categorias_df = pd.read_excel('inventario.xlsx', sheet_name='Categorías', header=0)
+    categorias_df['category_desc'] = categorias_df["Nombre"]
     
     # Concatenar category_id y subcategory_id en una nueva columna
-    categorias_df['category_code'] = categorias_df['category_id'].astype(str) + categorias_df['subcategory_id'].astype(str)
-    
+    categorias_df['category_code'] = categorias_df['category_id'].apply(lambda x: str(x).zfill(2)).astype(str)\
+        + categorias_df['subcategory_id'].apply(lambda x: str(x).zfill(2)).astype(str)
     # Guardar en hoja "Categorías" y "Inventario General"
-    categorias_df.to_excel('inventario.xlsx', sheet_name='Categorías', index=False)
-    df['category_code'] = categorias_df['category_code']
-    df.to_excel('inventario.xlsx', sheet_name='Inventario General', index=False)
+    test_df = pd.merge(df, categorias_df, on="category_desc", how="left")
 
-#insertar el mensaje "ANTES DE USAR ESTE ELEMENTO DEBES ESCANEAR ESTE CÓDIGO" si la columna 'type'=="large"
-def insert_message():
-    pass
+    df["category_id"] = test_df["category_id"]
+    df["subcategory_id"] = test_df["subcategory_id"]
+    df["category_code"] = test_df["category_code_y"]
 
-#insertar tipo (large o small) como parametro de entrada para buscar es el codigo.
-#large =25x15 mm
-#asigna large si category == Cajas
-def insert_type():
-    pass
-
-#Depende del tipo de QR asignado en la funcion insert_type
-def set_type_qr():
-    pass
-
-#Cargar los codigos de categorias (category_cod) y codigo unico (col cod) ademas de asignar el tipo.
-def update_col():
-    pass
-
-def set_type():
-    pass
-
-## NOTA: asignar por filas y obtener por columnas 
+    return df, categorias_df
 
 if __name__=="__main__":
 	# Lee el Excel
@@ -61,6 +48,13 @@ if __name__=="__main__":
     #Crear columna 'urls' con la combinación sitio web + code
     df["url"] = df["code"].apply(lambda x: 'astrosuite.firebaseapp.com/i/' + x)
 
+    [df, categorias_df] = insert_code_category(df)
+
+    #TODO: Finish, identify large and none qr meritory categories
+    df["type"] = df["category_code"].map({ "0900": "large", "0006": "none"}).fillna("small")
+
+    df["message"] = df["type"].map({ "large": "ESCANEA ESTE QR ANTES DE USAR" })
+
     # Guarda el archivo en Excel
     df.to_excel('exported.xlsx')
 
@@ -68,6 +62,6 @@ if __name__=="__main__":
     # TODO: debería recibir el Dataframe y usar las columnas title, type, owner y message 
     # para generar un PDF de los small (imagenes con qr y owner de tamaño 1x1.5cm) y los 
     # large (imagenes con qr, owner, title truncado, message y logo de tamaño 7.5x2.5cm)
-    generar_pagina(df["url"].to_list())
+    generar_pagina(df)
 else:
 	print("Modulo Importado: [", os.path.basename(__file__), "]")
